ecc658f45608bd8d78fa1dd33cdf2645f09d20b5
Иван Монастырный <floiz777@mail.ru>
2023-11-22 23:26:12 +1000

    refactoring

--- index.php
+++ index.php
@@ -1,24 +1,99 @@
 <?php
 include('inc/inc.php');
-$quere = "SELECT * FROM `teacher`";
-$res = $db->query("$quere",1);
-if ($res)
-{
-    echo'<table cellspacing=5 cellpadding=5>
-              <tr>
-                    <th>Преподаватель</th>
-                    <th>Часы</th>
-              </tr>';
-}
-foreach ($res as $r) {
 
-    echo "
-              <tr valign='top' style='border-bottom:ridge'>
-                    <td align='left'>$r[Преподаватель]</td>
-                    <td style='text-align:left;'><span style='word-wrap:break-word; float:left; max-width:130px;'>$r[часы]</span></td>
-                    <td align='center'>
-                    </td>
-                  </tr>";
+function get_sql_list($query) {
+    global $db;
+    return array_map(function ($res) { return $res[0]; }, $db->query($query));
 }
-echo '</table>';
 
+if ($_FILES) {
+    $hoursIndex = 2;
+    $filename = $_FILES["file"]["tmp_name"];
+    $data = array();
+    if (($handle = fopen($filename, "r")) !== FALSE) {
+        while (($row = fgetcsv($handle, 1000, "\t")) !== FALSE) {
+            foreach ($row as $index => &$item) {
+                $item = trim($item);
+                if ($index == $hoursIndex)
+                    $item = floatval($item);
+            }
+            $data[] = $row;
+        }
+        fclose($handle);
+    }
+    $db->query("TRUNCATE TABLE `data`");
+    foreach ($data as $row) {
+        $discipline = $db->escape($row[0]);
+        $hours = $db->escape($row[2]);
+        $teachers = $db->escape($row[3]);
+        $group = $db->escape($row[4]);
+        $db->query("INSERT INTO `data` (`discipline`, `hours`, `teacher`, `group`) VALUES ('$discipline', '$hours','$teacher','$group')");
+    }
+}
+
+$teachers = get_sql_list('SELECT DISTINCT `teacher` FROM `data` ORDER BY `teacher`');
+$groups = get_sql_list('SELECT DISTINCT `group` FROM `data` ORDER BY `group`');
+
+$selectedTeacher = $_GET['teacher'];
+$selectedGroup = $_GET['group'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<div>
+    <form action="" method="get">
+        <select name="group" onchange="this.form.submit()">
+            <option disabled <?= empty($selectedGroup) ? "selected" : "" ?>>Выберите группу</option>
+            <? foreach ($groups as $group) : ?>
+                <option value="<?= $group ?>" <?= !empty($group) && $group == $selectedGroup ? "selected" : "" ?>><?= $group ?></option>
+            <? endforeach; ?>
+        </select>
+    </form>
+    <br>
+    <form action="" method="get">
+        <select name="teacher" onchange="this.form.submit()">
+            <option disabled <?= empty($selectedTeacher) ? "selected" : "" ?>>Выберите преподавателя</option>
+            <? foreach ($teachers as $teacher) : ?>
+                <option value="<?= $teacher ?>" <?= !empty($selectedTeacher) && $teacher == $selectedTeacher ? "selected" : "" ?>><?= $teacher ?></option>
+            <? endforeach; ?>
+        </select>
+    </form>
+    <br>
+    <form action="./" enctype="multipart/form-data" method="post">
+        <input type="file" name="file">
+        <input type="submit" value="Отправить">
+    </form>
+</div>
+<br>
+<br>
+<div>
+    <?php
+    $teacher = $selectedTeacher;
+    $group = $selectedGroup;
+    if (!empty($teacher) || !empty($group)) {
+        $teacher = $db->escape($teacher);
+        $group = $db->escape($group);
+        $sql = "SELECT * FROM `data` WHERE " . (!empty($group) ? "`group` = '$group'" : "`teacher` = '$teacher'");
+        $data = $db->query($sql, MYSQLI_BOTH);
+        ?>
+        <table>
+            <tr>
+                <th><?= !empty($teacher) ? "Преподаватель" : "Группа" ?></th>
+                <th>Предмет</th>
+                <th>Часы</th>
+            </tr>
+            <?php foreach ($data as $row) : ?>
+                <tr style='border-bottom:ridge'>
+                    <td><?= $row[!empty($teacher) ? 'teacher' : 'group'] ?></td>
+                    <td><?= $row['discipline'] ?></td>
+                    <td style='text-align:left;'><span style='word-wrap:break-word; float:left; max-width:130px;'><?= $row['hours'] ?></span></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php } ?>
+</div>
+</body>
+</html>
