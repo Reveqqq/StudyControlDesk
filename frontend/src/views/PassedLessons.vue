cc835a8ef1ba0de0cf8e0f36ada46069473c276f
Альберт Загибин <guardian_96@mail.ru>
2023-12-13 16:55:07 +1000

    add filed about the condact of lesson

--- frontend/src/views/PassedLessons.vue
+++ frontend/src/views/PassedLessons.vue
@@ -1,6 +1,32 @@
 <script setup>
 import HeaderComp from "@/components/HeaderComp.vue";
 import FooterComp from "@/components/FooterComp.vue";
+import {reactive} from 'vue';
+
+const state = reactive ({
+  passedLessons: [
+    {
+      title: "Математический анализ (лек.)",
+      groups: [
+        "Б9122-01.03.02сп",
+        "Б9122-01.03.02мкт",
+        "Б9122-01.03.02сцт",
+      ],
+      conducted: false, //статус что пара проведена
+      date: '13.12.2023'
+    },
+    {
+      title: "Математический кекс (прак.)",
+      groups: [
+        "Б9122-01.03.02сп"
+        
+      ],
+      conducted: false,
+      date: '14.12.2023'
+    },
+   ]
+})
+
 </script>
 
 <template>
@@ -17,28 +43,26 @@ import FooterComp from "@/components/FooterComp.vue";
           <th scope="col" class="define-border">Редактировать</th>
         </tr>
         </thead>
-        <tr>
-          <td>Математический анализ (лек.)</td>
-          <td>00.00.000</td>
-          <td>text</td>
+        <tr
+        v-for="lesson in state.passedLessons"
+        :key="lesson.id"
+        >
+          <td>{{lesson.title}}</td>
+          <td>{{lesson.date}}</td>
+          <td>
+            <div
+            style="padding: 1%; margin: 1%;"
+            v-for="group in lesson.groups"
+            :key="group.id"
+            >
+              {{ group }}
+            </div>
+          </td>
           <td class="define-border">
             <a href="#">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
               <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
               <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-              </svg>
-            </a>
-          </td>
-        </tr>
-        <tr>
-          <td>Математический анализ (прак.)</td>
-          <td>00.00.000</td>
-          <td>text</td>
-          <td class="define-border">
-            <a href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
               </svg>
             </a>
           </td>
